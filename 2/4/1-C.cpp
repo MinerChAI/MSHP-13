c553ef42831cf69d558c22dd03a5ad914eef9a20
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2019-02-09 17:49:11 +0300

    LOL

--- 2/4/1-C.cpp
+++ 2/4/1-C.cpp
@@ -1,1 +1,8 @@
-#include <
+#include <iostream>
+#include <iomanip>
+
+int main(){
+    double a, b;
+    std::cin >> a >> b;
+    std::cout << std::fixed << std::setprecision(5) << (a + b) * (a + b) / 11;
+}
