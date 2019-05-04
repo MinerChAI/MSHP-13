038dbaf652110a191b9b6ac100ed43cc16f26dd0
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2019-05-04 17:59:09 +0300

    .

---
+++ 2/15/1-A.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(){
+    int n, k;
+    scanf("%d", &n);
+    int* arr = new int[n];
+    for (int i = 0; i < n; ++i) scanf("%d", arr + i);
+    scanf("%d", &k);
+    printf(arr[--k] > arr[k + 1] ? "YES" : "NO"); 
+}
---
+++ 2/15/1-B.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <algorithm>
+
+int main(){
+    int n, k;
+    scanf("%d", &n);
+    int* arr = new int[n];
+    for (int i = 0; i < n; ++i) scanf("%d", arr + i);
+    scanf("%d", &k);
+    if (!(arr[--k] > arr[k + 1] ? true : false) != true) std::swap(arr[k], arr[++k]);
+    for (int i = 0; i < n; ++i) printf("%d ", i[arr]);
+}
